deeb64ceebbaee49ce9201ad44f38a1c799fb3bb
Joseph Daughdrill <joseph.ldaughdrill@gmail.com>
2021-05-28 20:22:17 -0500

    Dockerized app

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM python:3.9.5-alpine
+
+# Set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# Set work directory
+RUN mkdir /app
+WORKDIR /app
+
+# Install dependencies
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-deps gcc libc-dev linux-headers postgresql-dev
+COPY ./requirements.txt /app/requirements.txt
+RUN pip install -r /app/requirements.txt
+
+# Remove dependencies
+RUN apk del .tmp-build-deps
+
+# Copy project
+COPY ./src /app
+
+# [Security] Limit the scope of user who run the docker image
+#RUN adduser -D user
+
+#USER user
--- src/bug_tracker/settings.py
+++ src/bug_tracker/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+print(os.environ.get('DB_USER'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,8 +77,11 @@ WSGI_APPLICATION = 'bug_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
