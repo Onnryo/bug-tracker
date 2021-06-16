d2ee9cb261059828634a7f034f880953c683411e
Joseph Daughdrill <joseph.ldaughdrill@gmail.com>
2021-06-16 14:35:48 -0500

    Added user login and registration

--- src/bug_tracker/settings.py
+++ src/bug_tracker/settings.py
@@ -32,7 +32,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'dashboard.apps.DashboardConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,3 +131,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_REDIRECT_URL = 'dashboard-home'
+LOGIN_URL = 'login'
--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index, name="dashboard-home"),
     path('about/', views.about, name="dashboard-about"),
