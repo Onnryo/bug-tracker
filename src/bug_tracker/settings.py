2054121e54ff5bf555a8987dc69c7a80e2b4b32b
Joseph Daughdrill <joseph.ldaughdrill@gmail.com>
2021-06-17 03:55:14 -0500

    User profile update forms

--- src/bug_tracker/settings.py
+++ src/bug_tracker/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'dashboard.apps.DashboardConfig',
     'crispy_forms',
     'crispy_bootstrap5',
+    'django_cleanup',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -18,4 +18,23 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        user_update_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            user_update_form.save()
+            profile_update_form.save()
+            messages.success(request, f'Your accout has been updated!')
+            return redirect('profile')
+    else:
+        user_update_form = UserUpdateForm(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+
+    ctx = {
+        'user_update_form': user_update_form,
+        'profile_update_form': profile_update_form,
+    }
+
+    return render(request, 'users/profile.html', context=ctx)
